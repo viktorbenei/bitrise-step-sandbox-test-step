99c4101760c8d109f7e6634cec0003ba75c3f6ce
Viktor Benei <viktor.benei@gmail.com>
2018-12-12 14:07:58 +0100

    pwd command output added

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 )
 
@@ -11,5 +12,16 @@ func main() {
 	fmt.Println("=========================")
 	fmt.Println(strings.Join(os.Environ(), "\n---\n"))
 	fmt.Println("=========================")
+
+	cmdLog, err := exec.Command("pwd").CombinedOutput()
+	if err != nil {
+		fmt.Printf("Failed to run command, error: %#v | output: %s", err, cmdLog)
+		os.Exit(1)
+	}
+
+	fmt.Println()
+	fmt.Printf("'pwd' command output: (%s)", cmdLog)
+	fmt.Println()
+
 	os.Exit(0)
 }
